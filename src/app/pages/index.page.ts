55447e4b5a41f31d04549723cc0d19bddff4e99f
Ales Rechtorik <foxxcz@gmail.com>
2024-11-14 21:03:51 +0100

    wip

--- src/app/pages/index.page.ts
+++ src/app/pages/index.page.ts
@@ -10,24 +10,27 @@ import { NgOptimizedImage } from '@angular/common'
     NgOptimizedImage
   ],
   template: `
+
     <h1 class="text-3xl font-bold text-white-900 pb-4">Analog.js x Spartan x Medusa.js x Zerops.io</h1>
 
     @for (item of products()?.products; track item.id) {
       <div class="mb-6">
+
         <h3 class="text-xl font-semibold text-white-800 mb-2">{{ item.title }}</h3>
+
         @if (item.thumbnail) {
-          <div class="relative">
-            <img
-              [ngSrc]="item.thumbnail"
-              [alt]="item.title"
-              width="600"
-              height="600"
-              class="rounded-lg shadow-md hover:shadow-xl transition-shadow duration-300"
-            />
-          </div>
+          <img
+            [ngSrc]="item.thumbnail"
+            [alt]="item.title"
+            [width]="600"
+            [height]="600"
+            class="rounded-lg shadow-md hover:shadow-xl transition-shadow duration-300"
+          />
         }
+
       </div>
     }
+
   `,
   styles: `
 
